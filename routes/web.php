c42f4457e8c692e3282da5dcd3763685dcaecce6
walugembe expedicto <54406688+Billy004@users.noreply.github.com>
2024-09-02 15:44:18 +0300

    fine tunning UI

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/',[App\Http\Controllers\categorycontroller::class, 'index']);
 Route::get('categories',[App\Http\Controllers\categorycontroller::class, 'index']);
 Route::get('categories/create',[App\Http\Controllers\categorycontroller::class, 'create']);
 Route::post('categories/create',[App\Http\Controllers\categorycontroller::class, 'store']);
 Route::get('categories/{id}/edit',[App\Http\Controllers\categorycontroller::class, 'edit']);
 
-Route::get('/', function () {
-    return view('frontend.index');
-});
+// Route::get('/', function () {
+//     return view('frontend.index');
+// });
